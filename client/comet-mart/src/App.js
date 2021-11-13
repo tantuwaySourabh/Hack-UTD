8dbdb6c33e8e0ac0d477bf46e8e117d607ba03fd
Srushti Sachdev <43631563+srushtiss@users.noreply.github.com>
2021-11-13 16:08:24 -0600

    Feature/srushti (#1)
    * initial commit
    
    * added backend pipeline

---
+++ client/comet-mart/src/App.js
@@ -0,0 +1,39 @@
+import {useEffect} from 'react'
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+
+  useEffect(() => {
+    
+    fetch('/users').then(resp => {
+        return resp.json();
+    }).then(data => {
+        console.log(data);
+    }).
+    catch(e => {
+      console.log(e)
+    });
+
+  }, [])
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
