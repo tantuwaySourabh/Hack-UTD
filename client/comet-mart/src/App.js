05cf732a8498f30d521f8ed4336501422d8db074
Srushti Sachdev <43631563+srushtiss@users.noreply.github.com>
2021-11-13 20:24:38 -0600

    Feature/srushti (#2)
    * initial commit
    
    * added backend pipeline
    
    * component login sign up

--- client/comet-mart/src/App.js
+++ client/comet-mart/src/App.js
@@ -1,39 +1,34 @@
 import {useEffect} from 'react'
 import logo from './logo.svg';
 import './App.css';
+import { makeStyles } from '@material-ui/core/styles';
+import { CssBaseline } from '@material-ui/core';
+import {Button} from '@mui/material';
+import{Router} from 'react-router';
+import PropTypes from 'prop-types';
+import Home from './components/Home';
 
-function App() {
+function App(){
 
-  useEffect(() => {
+
+   useEffect(() => {
     
     fetch('/users').then(resp => {
         return resp.json();
     }).then(data => {
         console.log(data);
-    }).
-    catch(e => {
+    }).catch(e => {
       console.log(e)
     });
 
-  }, [])
+   }, [])
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div class="App">
+        <Home></Home>
     </div>
   );
+
+
 }
 
 export default App;
